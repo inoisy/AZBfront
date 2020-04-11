f1f57824357861c86efa63cf80dd7ff8a0f2bdb7
inoisy <inoisy@bk.ru>
2020-04-11 21:05:09 +0300

    fix sitemap

--- routes.js
+++ routes.js
@@ -1,5 +1,49 @@
 const axios = require('axios')
 const backendUrl = process.env.BACKEND_URL || "https://api.azb-es.ru"
+const apolloFetch = require('apollo-fetch');
+
+async function getAllProducts() {
+  const {
+    data: productsCount
+  } = await axios.get(backendUrl + '/products/count')
+  console.log("routes -> productsCount", productsCount)
+  let productsArr = []
+  for (let i = 0; i < Math.ceil(productsCount / 1000); i++) {
+    console.log("getAllProducts => start Fetching", i * 1000)
+    // const uri = backendUrl + '/graphql'
+    const query = `
+            {
+                products(limit: 1000, start:${i*1000}) {
+                    id
+                    name
+                    SKU
+                    slug
+                  
+                }
+            }
+        `
+    const fetchApollo = apolloFetch.createApolloFetch({
+      uri: backendUrl + '/graphql'
+    });
+
+    const {
+      data
+    } = await fetchApollo({
+      query
+    })
+    let products = data.products
+    // console.log("getAllProducts -> fetched", products.length)
+    productsArr.push(...products)
+    // const {
+    //     data: products
+    // } = await axios.get(`${backendUrl}products?manufacturer=5d0749375b010404fca8dbca&_limit=1000&_start=${i*1000}`)
+    // console.log("deleteAllProducts -> products", products)
+
+  }
+  return productsArr
+}
+
+
 async function routes() {
   // console.log("sitemap-generation")
 
@@ -36,19 +80,22 @@ async function routes() {
       routes.push(`/catalog/${category.slug}/${subcategory.slug}`)
     }
   }
-  const {
-    data: productsCount
-  } = await axios.get(backendUrl + '/products/count')
-  console.log("routes -> productsCount", productsCount)
-  for (let i = 0; i < Math.ceil(productsCount / 1000); i++) {
-    console.log("start", i * 1000)
-    const {
-      data: products
-    } = await axios.get(`${backendUrl}/products?_limit=1000&_start=${i*1000}`)
-    for (let product of products) {
-      routes.push(`/product/${product.slug}`)
-    }
-  }
+
+  const products = await getAllProducts()
+  routes.push(...products)
+  // const {
+  //   data: productsCount
+  // } = await axios.get(backendUrl + '/products/count')
+  // console.log("routes -> productsCount", productsCount)
+  // for (let i = 0; i < Math.ceil(productsCount / 1000); i++) {
+  //   console.log("start", i * 1000)
+  //   const {
+  //     data: products
+  //   } = await axios.get(`${backendUrl}/products?_limit=1000&_start=${i*1000}`)
+  //   for (let product of products) {
+  //     routes.push(`/product/${product.slug}`)
+  //   }
+  // }
   console.log('routesLength', routes.length)
   //   const {
   //     data: products
