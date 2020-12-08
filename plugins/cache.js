39e0717c70a5f6e255ee98d969872a2e12f8f153
inoisy <inoisy@bk.ru>
2020-12-09 00:21:08 +0300

    fix

--- plugins/cache.js
+++ plugins/cache.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 const backendUrl = process.env.BACKEND_URL || `https://api.azb-es.ru`
 // import query from "../generalInfo"
 // console.log("🚀 ~ file: cache.js ~ line 5 ~ query", query)
-
+const axios = require("axios")
 const MyPlugin = {
   install(Vue, options) {
     Vue.prototype.$cached
@@ -12,45 +12,51 @@ const MyPlugin = {
         console.log("cached")
         return Vue.prototype.$cached
       } else {
-        console.log("get data from API")
-        const query = `
-          {
-            categories(where: { ismain: true }, sort: "name:asc") {
-              id
-              name
-              slug
-              child {
-                id
-                name
-                slug
-              }
-            }
-            contact {
-              name
-              content
-              email
-              phone
-              address
-            }
-            manufacturers {
-              id
-              name
-              slug
-              img {
-                url
-              }
-            }
-          }
-        `
-        const fetchApollo = apolloFetch.createApolloFetch({
-          uri: backendUrl + '/graphql'
-        });
+        const { data: categories } = await axios.get(backendUrl + "/categories?ismain=true&_sort=name:asc")
+        const { data: manufacturers } = await axios.get(backendUrl + "/manufacturers")
+        const { data: contacts } = await axios.get(backendUrl + "/contacts")
 
-        const {
-          data: dataFetched
-        } = await fetchApollo({
-          query
-        })
+        const dataFetched = { categories, manufacturers, contact: contacts }
+        // console.log("🚀 ~ file: cache.js ~ line 19 ~ dataFetched", dataFetched)
+        // console.log("get data from API")
+        // const query = `
+        //   {
+        //     categories(where: { ismain: true }, sort: "name:asc") {
+        //       id
+        //       name
+        //       slug
+        //       child {
+        //         id
+        //         name
+        //         slug
+        //       }
+        //     }
+        //     contact {
+        //       name
+        //       content
+        //       email
+        //       phone
+        //       address
+        //     }
+        //     manufacturers {
+        //       id
+        //       name
+        //       slug
+        //       img {
+        //         url
+        //       }
+        //     }
+        //   }
+        // `
+        // const fetchApollo = apolloFetch.createApolloFetch({
+        //   uri: backendUrl + '/graphql'
+        // });
+
+        // const {
+        //   data: dataFetched
+        // } = await fetchApollo({
+        //   query
+        // })
         if (dataFetched) {
           Vue.prototype.$cached = dataFetched
           return dataFetched
